98d53967609c584b552d5ca2c1a7fed3f4b4d4d0
deivdf <dfdavidfernando@gmail.com>
2025-03-25 07:19:05 -0500

    add: Chapeter Screen

---
+++ src/screens/manga/ChapeterReaderScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const ChapeterReaderScreen = () => {
+  return (
+    <View>
+      <Text>Chapter Reader Screen</Text>
+    </View>
+  );
+};
+
+export default ChapeterReaderScreen;
